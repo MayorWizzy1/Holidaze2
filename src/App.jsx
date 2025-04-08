f845ec2daa40e3cc134ea1476e58a76c88b26c77
H-chai <143179912+H-chai@users.noreply.github.com>
2025-04-08 10:55:20 +0200

    Add Route (login page, register page)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </div>
   );
