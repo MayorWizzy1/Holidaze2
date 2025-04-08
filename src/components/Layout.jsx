500bc8c1bde32b685f97ac58797aa50c425f39b8
H-chai <143179912+H-chai@users.noreply.github.com>
2025-04-08 14:38:43 +0200

    Not display Header and Footer on register or login page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from './Header/Header';
 import { Footer } from './Footer';
 
 export function Layout() {
+  const location = useLocation();
+
+  if (location.pathname === '/login' || location.pathname === '/register')
+    return <Outlet />;
   return (
     <div>
       <Header />
