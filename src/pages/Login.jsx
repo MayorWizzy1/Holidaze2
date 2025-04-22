a1d1a89ba388cd41b8e5f0e253f9974bbc817e5a
H-chai <143179912+H-chai@users.noreply.github.com>
2025-04-22 13:52:45 +0200

    Style login page (responsive)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -53,6 +53,7 @@ export function Login() {
       if (response.ok) {
         localStorage.setItem('token', result.data.accessToken);
         localStorage.setItem('username', result.data.name);
+        navigate('/');
       } else {
         setIsError(result.errors[0].message);
       }
@@ -60,30 +61,52 @@ export function Login() {
       setIsError(error.message);
     } finally {
       setIsLoading(false);
-      navigate('/');
     }
   }
 
   return (
-    <div>
-      <Link to="/">
-        <img src={logo} alt="Holidaze logo" />
+    <div className="px-6 font-roboto flex flex-col text-black h-svh justify-center">
+      <Link to="/" className="block w-1/2 mx-auto sm:w-1/3 md:w-1/4 lg:w-[14%]">
+        <img src={logo} alt="Holidaze logo" className="w-full" />
       </Link>
-      <h1>Sign in</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('email')} />
-        <p>{errors.email?.message}</p>
-        <input {...register('password')} />
-        <p>{errors.password?.message}</p>
+      <h1 className="text-center text-2xl font-semibold mt-8 mb-5 lg:mt-10 lg:mb-6">
+        Sign in
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="sm:w-3/5 sm:mx-auto md:w-1/2 xl:w-1/4"
+      >
+        <input
+          {...register('email')}
+          type="email"
+          placeholder="Email"
+          className="border border-outline placeholder:text-sm py-3.5 px-4 rounded-[10px] w-full"
+        />
+        <p className="text-error mt-1">{errors.email?.message}</p>
+        <input
+          {...register('password')}
+          type="password"
+          placeholder="Password"
+          className="border border-outline placeholder:text-sm py-3.5 px-4 rounded-[10px] w-full mt-4"
+        />
+        <p className="text-error mt-1">{errors.password?.message}</p>
 
-        {isError && <p style={{ color: 'red' }}>{isError}</p>}
+        {isError && <p className="text-error mt-4">{isError}</p>}
 
-        <button type="submit" disabled={isLoading}>
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="text-center bg-blue text-white block w-full rounded-[10px] py-3.5 font-medium mt-4 cursor-pointer transition-all duration-300 hover:bg-white hover:text-blue hover:border"
+        >
           {isLoading ? 'Sign in...' : 'Sign in'}
         </button>
       </form>
-      <p>Don’t have an account yet?</p>
-      <Link to="/register">Sign up now</Link>
+      <div className="flex text-sm justify-center mt-4">
+        <p className="text-outline mr-1">Don’t have an account yet?</p>
+        <Link to="/register" className="underline font-medium">
+          Sign up now
+        </Link>
+      </div>
     </div>
   );
 }
