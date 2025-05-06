05f14e84184e045db8be082a293a338a65704c9b
MayorWizzy1 <155782578+MayorWizzy1@users.noreply.github.com>
2025-05-06 14:47:34 +0100

    Update HeroSection.jsx
    Corrected an error I made by merging a pull from Stage branch

--- src/components/homePage/HeroSection.jsx
+++ src/components/homePage/HeroSection.jsx
@@ -4,8 +4,7 @@ export function HeroSection({ setSearchResults, setSearchText }) {
   return (
     <div className="bg-[url('/public/bg-hero.jpg')] bg-cover bg-center font-roboto">
       <div className="bg-bg-black px-10 py-12 sm:px-24 lg:py-24 lg:px-32">
-        <h1 className="font-boska text-yellow-400 text-[32px] mb-2 text-center font-bold lg:text-[40px] lg:text-left">
-           {/* Changed text color to yellow-400 */}
+        <h1 className="font-boska text-white text-[32px] mb-2 text-center font-bold lg:text-[40px] lg:text-left">
           Find your next stay
         </h1>
         <SearchForm
